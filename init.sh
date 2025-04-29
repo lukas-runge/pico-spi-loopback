56bd9545ea267c8a19d0f32567d8ef62ba55c606
Lukas Runge <login@lukasrunge.de>
2025-04-29 16:47:53 +0200

    build: :hammer: add helper scripts

---
+++ init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git submodule update --init --recursive &&
+mkdir -p build/picotool &&
+cd build/picotool &&
+printf "\n--- CONFIGURING ---\n" &&
+cmake -DPICO_SDK_PATH=../../pico-sdk ../../picotool &&
+printf "\n--- BUILDING ---\n" &&
+make -j14 &&
+cd ../..
